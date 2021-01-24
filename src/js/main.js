752a3e8c5ab8a21eddcbe5d517bdadf282fe0548
valentin-cochin <cochin.valentin@hotmail.fr>
2021-01-24 21:40:15 +0100

    Add modal to introduce the game

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,37 @@
-$( document ).ready(function() {
+"use strict"
+
+var introModal;
+
+
+$(document).ready(function () {
+    // afficher modal
+    // introModal = new bootstrap.Modal(document.getElementById('exampleModal'));
+    // introModal.show();
+    $('#introModal').modal('show');
+
+
+    // Charger fichier Json
+
+    // Randomiser couleurs
+
+    // Afficher drapeau selon niveau
+
+    // Lancer timer quand cliqué
+
+    // arrêter timer
+
+    // ajouter score
+
+    // afficher modal
+
+    // passer au niveau suivant
+
+    // transitionvers dtrapeau
+
+    // timer à zéro
+
+    // Si dernier drapeau
+
+    // Affichage du score
 });
+
